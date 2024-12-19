6d4b831ffa7c6ef81213f714bfe92e9449c5047e
Luciano <lucaaron00@gmail.com>
2024-12-19 00:36:15 -0600

    Chars in the byte board

--- Board.java
+++ Board.java
@@ -1,30 +1,37 @@
 public class Board {
     
-    private byte[][] table;
+    private char[][] table;
 
     public Board(){
-        this.table = new byte[3][3];
+        this.table = new char[3][3];
 
         // Initialize the board with numbers 1 to 9
-        byte counter = 1;
-        for(byte row = 0; row <= 2; row++){
-            for(byte column = 0; column <= 2; column++){
-                this.table[row][column] = counter++;
-            }
-        }
-
+        table[0] = new char[]{'1','2','3'};
+        table[1] = new char[]{'4','5','6'};
+        table[2] = new char[]{'7','8','9'};
+        
     }
 
-    public char getCell(byte cellNumberX, byte cellNumberY){
+    public char getCell(int cellNumberX, int cellNumberY){
         return (char) this.table[cellNumberX][cellNumberY];
     }
 
     public void printBoard(){
-        for (byte[] row : table) {
-            for (byte column : row) {
+        for (char[] row : table) {
+            for (char column : row) {
                 System.out.print(String.format("[%s]", column));
             }
             System.out.println();
+        }
+    }
+
+    public void setPlayerInCell(char playerInput){
+        for(int row = 0; row <= 2; row++){
+            for (int col = 0; col <= 2; col++) {
+                if (this.table[row][col] == playerInput) {
+                    this.table[row][col] = playerInput;
+                }
+            }
         }
     }
     
--- Game.java
+++ Game.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Game {
     
-    public static char requestPlayerName(){
+    private static char requestPlayerName(){
         Scanner input = new Scanner(System.in);
         System.out.print("Player 1 = ");
         char aPlayer = input.next().charAt(0);
@@ -10,15 +10,30 @@ public class Game {
         return aPlayer;
     }
 
+    private static void playerInput(Player aPlayer){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Player" + aPlayer.getName() + "turn = ");
+        
+    }
+
     public static void play(){
         // Make the instance for the players
         Player player1 = new Player(requestPlayerName());
         Player player2 = new Player(requestPlayerName());
 
-        // Beggin the turnovers
+        Board aBoard = new Board();
+
+        // Start the game
         Boolean victory = false;
         while (victory == false) {
             
+            
+
+            
+            // check the winner
+
+
+
         }
 
 
